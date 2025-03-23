04ac3dc272295aff2bce26d723b8d600b1fc681b
Wilfried71 <tpeyrot71@gmail.com>
2025-03-23 16:30:05 -0400

    Init

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	discovery "github.com/Thomas-PEYROT/discovery-client"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	// Enregistrement du microservice
+	discovery.RegisterMicroservice("service-a")
+
+	// Création du routeur Gin
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		log.Println("Called here !")
+		c.String(http.StatusOK, "Hello microservice A !")
+	})
+
+	// Démarrage du serveur dans une goroutine
+	port := discovery.ServiceInformations.Port
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: router,
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Erreur lors du démarrage du serveur : %v", err)
+		}
+	}()
+
+	// Capture des signaux système pour exécuter le code avant la fermeture
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	// Attente du signal d'arrêt
+	<-quit
+	log.Println("Arrêt du microservice...")
+
+	// Code de nettoyage avant l'arrêt
+	discovery.UnregisterMicroservice() // Exemple d'une éventuelle fonction de nettoyage
+	log.Println("Microservice arrêté proprement.")
+}
